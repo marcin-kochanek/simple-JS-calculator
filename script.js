0cbf00812b49fabfeaf8e1800a4b917195796200
Marcin Kochanek <md.kochanek@gmail.com>
2018-03-01 00:17:55 +0100

    Add a function to numbers

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 
-let clickerButtons = document.querySelectorAll('.btn');
+const allButtons = document.querySelectorAll('.btn');
+const numberButtons = document.querySelectorAll('.btn--number');
 
-Array.from(clickerButtons).forEach(btn => {
-  btn.addEventListener('click', function() {
-    let sound = new Audio();
+function clickedNumber() {
+  let currentNumber = this.textContent;
+
+  document.getElementById('result').value += currentNumber;
+}
+
+Array.from(allButtons).forEach(btn => {
+  btn.addEventListener('click', () => {
+    const sound = new Audio();
     
     sound.src = "https://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/tink.wav";
     sound.play();
   });
+});
+
+Array.from(numberButtons).forEach(btn => {
+  btn.addEventListener('click', clickedNumber);
 });
