a5912100d4e255894c060b748e7dce9b4b66c08f
Marcin Kochanek <md.kochanek@gmail.com>
2018-03-02 01:03:32 +0100

    Create functions that are responsible for clearing result, changing the sign of a number (a plus/minus symbol), adding the dot to numbers

--- script.js
+++ script.js
@@ -1,22 +1,92 @@
-
+let firstNumber, secondNumber, isFirst = true, operator, clickedNumber;
+let outputValue = document.getElementById('result');
 const allButtons = document.querySelectorAll('.btn');
 const numberButtons = document.querySelectorAll('.btn--number');
+const clearButton = document.querySelector('.btn--clear');
+const dotButton = document.querySelector('.btn--dot');
+const plusMinusButton = document.querySelector('.btn--plusminus');
+const operatorButtons = document.querySelectorAll('.btn--operator');
+const resultButton = document.querySelector('.btn--equals');
 
-function clickedNumber() {
-  let currentNumber = this.textContent;
+function goodSound() {
+  const sound = new Audio();
+  
+  sound.src = "./tink.wav";
+  sound.play();
+}
 
-  document.getElementById('result').value += currentNumber;
+function wrongSound() {
+  const sound = new Audio();
+  
+  sound.src = "./wrong.wav";
+  sound.play();
+}
+
+function clearAll() {
+  outputValue.value = 0;
+  isFirst = true;
+}
+
+function addDot() {
+  if (!outputValue.value.includes('.')) {
+    outputValue.value += '.';
+  } else {
+    wrongSound();
+  }
+}
+
+function clickNumber() {
+  clickedNumber = this.value;
+  
+  if (isFirst || outputValue.value == "0") {
+    outputValue.value = clickedNumber;
+    isFirst = false;
+  } else {
+    outputValue.value += clickedNumber;
+    secondNumber = parseInt(outputValue.value);
+    console.log(secondNumber, clickedNumber);
+  }
+}
+
+function changeSign() {
+  outputValue.value *= -1;
+}
+
+function calculateResult() {
+  let result;
+  secondNumber = parseFloat(outputValue.value); // ustawia second jako to co wyswietla, poprzedni wynik
+  
+  if (operator == '+') {
+    result = firstNumber + secondNumber;
+  }
+
+  isFirst = true;
+  firstNumber = result;
+  outputValue.value = result;
+  console.log(firstNumber, secondNumber, result, clickedNumber);
+}
+
+function operatorClick() {
+  operator = this.value;
+
+  firstNumber = parseInt(outputValue.value);
+  console.log(firstNumber);
+  isFirst = true;
 }
 
 Array.from(allButtons).forEach(btn => {
-  btn.addEventListener('click', () => {
-    const sound = new Audio();
-    
-    sound.src = "https://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/tink.wav";
-    sound.play();
-  });
+  btn.addEventListener('click', goodSound);
 });
 
 Array.from(numberButtons).forEach(btn => {
-  btn.addEventListener('click', clickedNumber);
+  btn.addEventListener('click', clickNumber);
 });
+
+Array.from(operatorButtons).forEach(btn => {
+  btn.addEventListener('click', operatorClick);
+});
+
+clearButton.addEventListener('click', clearAll);
+dotButton.addEventListener('click', addDot);
+plusMinusButton.addEventListener('click', changeSign);
+resultButton.addEventListener('click', calculateResult);
