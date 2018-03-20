22e08b38a058e01acab46d587547e1934eafff59
Marcin Kochanek <md.kochanek@gmail.com>
2018-03-21 00:50:39 +0100

    Finishing touches

--- script.js
+++ script.js
@@ -17,8 +17,6 @@ function getNumberLength(number) {
 
   numberLength = 0;
 
-  console.log(stringifiedNumber);
-
   Array.from(stringifiedNumber).map(figure => {  
     if (Number.isInteger(parseInt(figure))) {
       numberLength++;
@@ -28,66 +26,21 @@ function getNumberLength(number) {
       return integerPart;
     }
   });
-  console.log(numberLength);
 }
 
 Math.decimal = function(result, k) {
   var factor;
 
   getNumberLength(result);
-  console.log(result);
   
-  ///TODO: popraw dla dzielenia
   if (integerPart < numberOfDisplayedFigures && numberLength >= numberOfDisplayedFigures) {
-    console.log(`result:${result}`);
     factor = Math.pow(10, k+1-integerPart);
-    console.log(`factor:${factor}`);
     result = Math.round(Math.round((result)*factor)/10);
-    console.log(`result:${result}`);
-    console.log(`return: ${result/(factor/10)}`);
-    return result/(factor/10);
-  } else {
-    console.log(`result:${result}`);
-    return result;
-  }
-};
-
-/*
-var result = 12345678.875728688, integerPart;
-
-function getNumberLength(result) {
-  let stringifiedNumber = result.toString(),
-      numberLength = 0, isNegative = 0;
-  
-  Array.from(stringifiedNumber).map(figure => {  
-    if (Number.isInteger(parseInt(figure))) {
-      numberLength++;
-    } else if (figure === '-') {
-      isNegative = -1;
-    } else if (figure === '.') {
-      integerPart = stringifiedNumber.indexOf(figure) + isNegative;
-    }
-  });
-}
-
-Math.decimal = function(result, k) {
-  var factor;
- 
-  getNumberLength(result);
-  
-  if (integerPart <= 8) {
-    factor = Math.pow(10, k+1-integerPart);  
-    result = Math.round(Math.round(result*factor)/10);
     return result/(factor/10);
   } else {
     return result;
   }
 };
-
-console.log(Math.decimal(result, 8));
-
-//isNumber = Number.isInteger(result.toString()[i]);
-*/
 
 function clearFromZero(x) {
   const findE = x.search('e');
@@ -106,8 +59,6 @@ function checkResult(result) {
   } else if (integerPart >= 9 || (integerPart == undefined && numberLength >= 9)) {
     n = result.toExponential(5);
     outputValue.value = clearFromZero(n);
- /* } else if (integerPart >= 1) {
-    outputValue.value = clearFromZero(result);*/
   } else {
     outputValue.value = result;
   }
@@ -152,11 +103,8 @@ function clearAll() {
 }
 
 function addDot() {
-  console.log(result);
-
   if ((!outputValue.value.includes('.') && !isNaN(result) && (secondNumber.toString().length < numberOfDisplayedFigures) && !isFirst) || !isFinite(result)) {
     outputValue.value += '.';
-    console.log(firstNumber, secondNumber, result);
   } else if ((result === "Error" || secondNumber === undefined || outputValue.value !== "0.") && isFirst/*isNewAction*/) {
     outputValue.value = "0.";
   } else {
@@ -164,7 +112,6 @@ function addDot() {
   }
 
   isFirst = false;
-  console.log(result);
 }
 
 function clickNumber() {
@@ -174,7 +121,6 @@ function clickNumber() {
     outputValue.value = clickedNumber;
     secondNumber = parseFloat(outputValue.value);
     isFirst = false;
-    //isNewAction = false; //*
   } else if ((outputValue.value === "-0" && clickedNumber === "0")) {
     outputValue.value = "-0";
     secondNumber = parseFloat(outputValue.value);
@@ -182,10 +128,7 @@ function clickNumber() {
   } else if (outputValue.value.length < numberOfDisplayedFigures) {
     outputValue.value += clickedNumber;
     !outputValue.value.includes('.') ? outputValue.value = parseFloat(outputValue.value) : '';
-    //outputValue.value < 0 ? outputValue.value = Math.abs(outputValue.value)*-1 : outputValue.value = parseFloat(outputValue.value);
-    //outputValue.value = parseFloat(outputValue.value);
     secondNumber = parseFloat(outputValue.value);
-    //secondNumber = outputValue.value;
   } else {
     playSound(false);
   }
@@ -195,10 +138,7 @@ function clickNumber() {
 }
 
 function showPercentage() {
-  /*if (isNaN(secondNumber)) {
-    secondNumber = outputValue.value = "Error";
-  } else {*/
-    secondNumber = outputValue.value /= 100;
+  secondNumber = outputValue.value /= 100;
 
   calculateResult();
   checkResult(secondNumber); // czy checkResult(result)
@@ -208,35 +148,18 @@ function showPercentage() {
 function changeSign() {
   if (isNaN(secondNumber) || secondNumber == '0' || (!isNewAction && isFirst)) {
     secondNumber = outputValue.value = '-0';
-    //isNewAction = false;
     isFirst = false;
-  /*} else {
-    outputValue.value = '-'.concat(outputValue.value);
-    secondNumber = outputValue.value *= -1;
-    //result *= -1;
-    calculateResult();
-    checkResult(secondNumber);
-  }*/
   } else {
     if (outputValue.value.charAt(0) === '-') {
       outputValue.value = outputValue.value.slice(1);
-      //outputNumbers.value = outputNumbers.value.slice(2, -1);
     } else {
       outputValue.value = '-'.concat(outputValue.value);
-      //outputNumbers.value = '-('.concat(outputNumbers.value, ')');
     }
 
     secondNumber = parseFloat(outputValue.value);
     calculateResult();
     checkResult(secondNumber);
-    //checkResult(result);
   }
-  
-  //checkResult(result);
-  //isFirst = false;
-
-  // zastosowanie metody toString, aby wyświetlić liczbę z minusem
-  //secondNumber = parseFloat(secondNumber);
 }
 
 function showResult() {
@@ -263,20 +186,15 @@ function operatorClick() {
     outputNumbers.value = firstNumber;
   } else {
     firstNumber = result;
-    //outputValue.value = result;
     checkResult(result);
   }
 
-  //secondNumber = undefined; // potrzebne do warunku w funckji change Sign?
   secondNumber = result;
   isFirst = true;
 }
 
 function showResultNumber() {
   const lastSign = [...outputNumbers.value][[...outputNumbers.value].length-1];
-  console.log(operator, lastSign);
-
-  //outputNumbers.value += secondNumber;
 
   if (isNumberClicked) {
     if (firstNumber === 0 || (equalButton === "=" && isNaN(lastSign))) {
@@ -290,8 +208,6 @@ function showResultNumber() {
     }
   }
   isNumberClicked = false;
-  //outputNumbers.value.slice((secondNumber.toString().length*-1), -1)
-  //&& Number.isInteger(outputNumbers.value.slice((secondNumber.toString().length*-1)))
 }
 
 // Nasłuchiwanie zdarzeń 'click'
